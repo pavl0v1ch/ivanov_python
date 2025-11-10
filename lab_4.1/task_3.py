15c6fc3143dabd79bb797a976d3fb708a2efff67
Татьяна <ivanovdanik776@gmail.com>
2025-11-10 09:15:02 +0300

    обезьяна

--- lab_4.1/task_3.py
+++ lab_4.1/task_3.py
@@ -11,7 +11,7 @@ beige = '#f5e6c8'
 black = '#000000'
 white = '#ffffff'
 
-# --- ГОЛОВА ---
+#  ГОЛОВА
 head = patches.Ellipse((0, 3), width=4, height=3.5, facecolor=gray)
 face = patches.Ellipse((0, 2.7), width=2.5, height=1.8, facecolor=beige)
 ax.add_patch(head)
@@ -36,17 +36,17 @@ ax.add_patch(patches.Ellipse((0.2, 2.9), 0.15, 0.1, facecolor=black))
 # Рот
 ax.add_patch(patches.Arc((0, 2.5), 1.2, 0.6, theta1=200, theta2=340, color=black, linewidth=2))
 
-# --- ТЕЛО ---
+# ТЕЛО
 body = patches.Ellipse((0, -0.5), width=3.5, height=4.5, facecolor=gray)
 belly = patches.Ellipse((0, -0.5), width=2.2, height=3.0, facecolor=beige)
 ax.add_patch(body)
 ax.add_patch(belly)
 
-# Руки (удлинены и ближе к телу)
+# Руки
 ax.add_patch(patches.Ellipse((-1.6, 0.5), 1.4, 0.5, angle=20, facecolor=gray))
 ax.add_patch(patches.Ellipse((1.6, 0.5), 1.4, 0.5, angle=-20, facecolor=gray))
 
-# Ладони (подняты выше и сдвинуты в стороны)
+# Ладони
 ax.add_patch(patches.Ellipse((-2.4, 0.2), 0.5, 0.3, facecolor=beige))  # левее
 ax.add_patch(patches.Ellipse((2.4, 0.2), 0.5, 0.3, facecolor=beige))   # правее
 
