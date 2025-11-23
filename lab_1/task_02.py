663360631d65d0ac20374366ca93b25c520ef9a9
pavl0v1ch <fyfyd85@gmail.com>
2025-11-23 11:57:12 +0300

    Delete lab_1 directory

--- lab_1/task_02.py
+++
@@ -1,9 +0,0 @@
-string = input("Введите строку:")
-
-if string.strip() == "":
-    print("Пустая строка")
-else:
-    vowels = "aeiouAEIOU"
-    table = str.maketrans("", "", vowels)
-    result = string.translate(table)
-    print("Результат:", result)
--- lab_1/task_07.py
+++
@@ -1,5 +0,0 @@
-sec = int(input("Введите количество секунд: "))
-min = sec // 60
-sec_left = sec % 60
-
-print(f"{min}:{sec_left}")
