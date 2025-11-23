9f8f850f24f78750dad7eb22ebe413502f5bee71
pavl0v1ch <fyfyd85@gmail.com>
2025-11-23 11:56:11 +0300

    Delete lab_1 directory

--- lab_1/task_01.py
+++
@@ -1,8 +0,0 @@
-familiya = input("Фамилия: ")
-imya = input("Имя: ")
-otch = input("Отчество: ")
-
-if familiya == "" or imya == "" or otch == "":
-    print("Пожалуйста, заполните все поля!")
-else:
-    print(f"Итоговое значение: {familiya} {imya[0]}.{otch[0]}.")
--- lab_1/task_05.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Введите число: "))
-
-if number % 7 == 0:
-    print("Магическое число!")
-else:
-    summa = sum(int(digit) for digit in str(number))
-    print("Сумма цифр:", summa)
